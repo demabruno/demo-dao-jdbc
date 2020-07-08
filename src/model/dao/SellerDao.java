9c464ee8a5a5c0511fbd47a78e6617ad87375a1f
Bruno Demartini <bruno.demartini@gmail.com>
2020-07-07 22:20:59 -0300

    Classes DAO

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.application.Seller;
+
+public interface SellerDao {
+	void insert (Seller obj);
+	void update (Seller obj);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+	void delete (Integer id);
+}
